0f28cdc8264b480f9de9825a910149c29ec08001
Astrid Yu <astrid@astrid.tech>
2022-07-25 12:58:04 -0700

    Update things to be dnf instead of apt

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -2,13 +2,18 @@
 # This script bootstraps a workstation by creating an Ansible user.
 
 bootstrap_workstation() {
-  set -e
-  set -o xtrace
+  # wrap this script in a function, so that it doesn't fail due to network issues during curl
+
+  set -e # fail on first error
+  set -o xtrace # print executed commands
 
   if [ "$EUID" -ne 0 ]; then
     echo "Must be run as root."
     exit 1
   fi
+
+  # This will undo the changes that this script makes.
+  # rm -rf ~ansible && userdel ansible
 
   echo "Creating Ansible user..."
   useradd ansible -mr -G wheel -c "Ansible Admin"
@@ -17,9 +22,10 @@ bootstrap_workstation() {
   echo '%ansible ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ansible-can-sudo-without-password
 
   echo "Installing Ansible's SSH key..."
-  SSH_TMP=$(mktemp)
-  echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ94eDys1WjAJXC5FU/iZsDApHwRIqNmO2Ptuh/hOJGc ansible' >> "$SSH_TMP"
-  install -D $SSH_TMP ~ansible/.ssh/authorized_keys
+  install -d 700 ~ansible/.ssh
+  echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ94eDys1WjAJXC5FU/iZsDApHwRIqNmO2Ptuh/hOJGc ansible' >> ~ansible/.ssh/authorized_keys
+  chmod 600 ~ansible/.ssh/authorized_keys
+  chown -R ansible ~ansible/.ssh
 }
 
 bootstrap_workstation
